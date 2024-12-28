da3eaf949f9ad118b206409c72999fed5fdf0b88
aishw <aishwarya.balyaya19@gmail.com>
2024-12-28 00:25:08 -0500

    Final Changes added

--- src/src/Components/Catalogue.java
+++ src/src/Components/Catalogue.java
@@ -6,26 +6,20 @@ import java.util.List;
 
 public class Catalogue implements Search{
     private HashMap<String, List<Book>> bookTitles;
-    private HashMap<String, List<Book>> bookAuthors;
-    private HashMap<String, List<Book>> bookSubjects;
-    private HashMap<String, List<Book>> bookPublicationDates;
+    private HashMap<String, List<Author>> bookAuthors;
+
+    public Catalogue(HashMap<String, List<Book>> bookTitles, HashMap<String, List<Author>> bookAuthors) {
+        this.bookTitles = bookTitles;
+        this.bookAuthors = bookAuthors;
+    }
+
     @Override
     public List<Book> searchByTitle(String title) {
         return bookTitles.get(title);
     }
 
     @Override
-    public List<Book> searchByAuthor(String author) {
+    public List<Author> searchByAuthor(String author) {
         return bookAuthors.get(author);
-    }
-
-    @Override
-    public List<Book> searchBySubject(String subject) {
-        return bookSubjects.get(subject);
-    }
-
-    @Override
-    public List<Book> searchByPubDate(Date publishDate) {
-        return bookPublicationDates.get(publishDate);
     }
 }
--- src/src/Components/Search.java
+++ src/src/Components/Search.java
@@ -5,7 +5,5 @@ import java.util.List;
 
 public interface Search {
     public List<Book> searchByTitle(String title);
-    public List<Book> searchByAuthor(String author);
-    public List<Book> searchBySubject(String subject);
-    public List<Book> searchByPubDate(Date publishDate);
+    public List<Author> searchByAuthor(String author);
 }
